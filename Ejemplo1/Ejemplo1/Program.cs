c4f82ee3021aadb5372603965d5a2784ff04e87e
Antonio <Antonio@DESKTOP-O-0>
2017-07-31 14:36:51 -0500

    Change 99

--- Ejemplo1/Ejemplo1/Program.cs
+++ Ejemplo1/Ejemplo1/Program.cs
@@ -9,8 +9,6 @@ namespace Ejemplo1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World one more time!");
             Console.WriteLine("New line of code");
-            Console.WriteLine("Hola profe");
-
             Console.Read();
         }
     }
