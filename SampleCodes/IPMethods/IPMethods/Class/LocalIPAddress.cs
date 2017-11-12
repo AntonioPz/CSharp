514881936d4d8e50a8d9e8d837a5f8d23d6849a7
Antonio Perez <Antonio_dddd@hotmail.es>
2017-11-11 22:27:45 -0600

    Upload

---
+++ SampleCodes/IPMethods/IPMethods/Class/LocalIPAddress.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IPMethods.Class
+{
+    class LocalIPAddress
+    {
+        private static List<Ping> pingers= new List<Ping>();
+        private static int instances = 0;
+        private static object @lock = new object();
+        private static int result = 0;
+        private static int timeOut = 250;
+        private static int ttl = 5;
+        
+        public static void RunIpAddressDetection(string BaseIP)
+        {
+            result = 0;
+            //string baseIP = "192.168.92.";
+
+            Console.WriteLine("Pinging 255 destinations of C-class in {0}*", BaseIP);
+
+            CreatePingers(255);
+
+            PingOptions po = new PingOptions(ttl, true);
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+            byte[] data = enc.GetBytes("abababababababababababababababab");
+
+            SpinWait wait = new SpinWait();
+            int cnt = 1;
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            foreach (Ping p in pingers)
+            {
+                lock (@lock)
+                {
+                    instances += 1;
+                }
+
+                p.SendAsync(string.Concat(BaseIP, cnt.ToString()), timeOut, data, po);
+                cnt += 1;
+            }
+
+            while (instances > 0)
+            {
+                wait.SpinOnce();
+            }
+
+            watch.Stop();
+
+            DestroyPingers();
+
+            Console.WriteLine("Finished in {0}. Found {1} active IP-addresses.", watch.Elapsed.ToString(), result);
+
+        }
+        public static void Ping_completed(object s, PingCompletedEventArgs e)
+        {
+            lock (@lock)
+            {
+                instances -= 1;
+            }
+
+            if (e.Reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine(string.Concat("Active IP: ", e.Reply.Address.ToString()));
+                result += 1;
+            }
+            else
+            {
+                //Console.WriteLine(String.Concat("Non-active IP: ", e.Reply.Address.ToString()))
+            }
+        }
+        private static void CreatePingers(int cnt)
+        {
+            for (int i = 1; i <= cnt; i++)
+            {
+                Ping p = new Ping();
+                p.PingCompleted += Ping_completed;
+                pingers.Add(p);
+            }
+        }
+        private static void DestroyPingers()
+        {
+            foreach (Ping p in pingers)
+            {
+                p.PingCompleted -= Ping_completed;
+                p.Dispose();
+            }
+
+            pingers.Clear();
+
+        }
+        public static void GetYourOwnIpAddress()
+        {
+            // Get host name
+            String strHostName = Dns.GetHostName();
+
+            // Find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Enumerate IP addresses
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine(strHostName + ": " + ipaddress.ToString());
+            }
+        }
+        public static void NetworkInfo()
+        {
+            foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                Console.WriteLine("Name: " + netInterface.Name);
+                Console.WriteLine("Description: " + netInterface.Description);
+                Console.WriteLine("Addresses: ");
+                IPInterfaceProperties ipProps = netInterface.GetIPProperties();
+                foreach (UnicastIPAddressInformation addr in ipProps.UnicastAddresses)
+                {
+                    Console.WriteLine(" " + addr.Address.ToString());
+                }
+                Console.WriteLine("______________________________________________________________________");
+            }
+        }
+    }
+}
---
+++ SampleCodes/IPMethods/IPMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IPMethods.Class;
+using System.Net;
+
+namespace IPMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //LocalIPAddress.RunIpAddressDetection("192.168.92.");
+
+                //LocalIPAddress.GetYourOwnIpAddress();
+
+                LocalIPAddress.NetworkInfo();
+                Console.WriteLine("######################################################################");
+                //Server myServer = new Server();
+                //myServer.GetServer(args);
+
+                Console.WriteLine("Press 'enter' for try again or, press 'c' and 'enter' for clear screen");                
+                var key= Console.ReadLine();
+                if (key=="c")
+                    Console.Clear();
+                    
+            }
+        }
+        
+    }
+}
